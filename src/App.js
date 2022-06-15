f1a86dd4f08b5f5a8eb4f40eea82480472a1160b
datastoreking <vladislavcukanovfree@gmail.com>
2022-06-16 02:36:56 +0300

    initial UI

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='address-balance'>
+          <select className='address'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <div className='balance'>balance: <span>100</span></div>
+        </div>
+        <div className='claim-connect'>
+          <button className='btn-claim-enable'>claim</button>
+          <button className='btn-connectwallet'>connect metamask</button>
+        </div>
+      </div>
     </div>
   );
 }
