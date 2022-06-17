fbdace92573d1e9125202155f14febf02394271c
datastoreking <vladislavcukanovfree@gmail.com>
2022-06-18 00:42:23 +0900

    updated frontend UI

--- src/App.js
+++ src/App.js
@@ -1,106 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import './ConnectMetamask'
 import './App.css';
+import Input from './Address_Balance.txt';
+import ConnectMetamask from "./ConnectMetamask";
+
+// make the array of all claimable addresses
+const claimableAddress_Array = [];
+
+fetch(Input)
+	.then((r) => r.text())
+	.then(text  => {
+		const lines = text.split("\n");
+		for(var line = 0; line < lines.length; line ++){
+			if(lines[line]){
+				const claimableAddress = lines[line].slice(0,42);
+				claimableAddress_Array.push(claimableAddress);
+			}
+		}
+		
+})
 
 function App() {
-
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState(null);
-  const [userAddress, setUserAddress] = useState("");
-  const [amount, setAmount] = useState();
-  const [toaddress, setToaddress] = useState("address-1");
-
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", accountsChanged);
-      window.ethereum.on("chainChanged", chainChanged);
-    }
-  }, []);
-
-  const connectHandler = async () => {
-    if (window.ethereum) {
-      try {
-        const res = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        await accountsChanged(res[0]);
-        const { ethereum } = window;
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        
-        const addr = await signer.getAddress();
-
-        setUserAddress(addr.toString());
-      } catch (err) {
-        console.error(err);
-        setErrorMessage("There was a problem connecting to MetaMask");
-      }
-    } else {
-      setErrorMessage("Install MetaMask");
-    }
-  };
-
-  const accountsChanged = async (newAccount) => {
-    setAccount(newAccount);
-    try {
-      const balance = await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [newAccount.toString(), "latest"],
-      });
-      setBalance(ethers.utils.formatEther(balance));
-    } catch (err) {
-      console.error(err);
-      setErrorMessage("There was a problem connecting to MetaMask");
-    }
-  };
-
-  const chainChanged = () => {
-    setErrorMessage(null);
-    setAccount(null);
-    setBalance(null);
-  };
-
-  // useEffect(() => {
-  //   connectHandler()
-  // },[]);
-
-  const getInputValue = (event)=>{
-    // show the user input value to console
-    const userValue = event.target.value;
-    setAmount(userValue);
-    
-  };
-
-
-  const claimHandle = () => {
-    console.log(amount);
-    console.log(toaddress);
-  }
-
+  
   return (
     <div className="App">
-      <div>
-        <div className='address-balance'>
-          <select className='address' onChange={(e) => setToaddress(e.target.value)}>
-            <option value="address-1">address-1</option>
-            <option value="address-2">address-2</option>
-            <option value="address-3">address-3</option>
-            <option value="address-4">address-4</option>
-            <option value="address-5">address-5</option>
-          </select>
-          <div className='balance'>balance: <span>100</span></div>
-        </div>
-        <div className='claim-connect'>
-          <input type="text" className="amount" onChange={getInputValue}/>
-          {
-            account != null ? <button className='btn-claim-enable' onClick={claimHandle}>claim</button> : <button className='btn-claim-disabled' disabled>claim</button>
-          }
-          
-        </div>
-        {
-          account == null ? <button className='btn-connectwallet' onClick={connectHandler}>connect metamask</button> : <button className='btn-connectwallet' onClick={connectHandler}>{userAddress}</button>
-        }
+      <ConnectMetamask />
+      <div className="claimableAddress">
+        <h1>Claimable Addresses and their balances:</h1>
+        <p>{claimableAddress_Array[0]}: <span> 1000</span></p>
+        <p>{claimableAddress_Array[1]}: <span> 1000</span></p>
+        <p>{claimableAddress_Array[2]}: <span> 1000</span></p>
+        <p>{claimableAddress_Array[3]}: <span> 1000</span></p>
+        <p>{claimableAddress_Array[4]}: <span> 1000</span></p>
+        <h1>Current Smart Contract Balance:</h1>
+        <p className="contractBalance">10000</p>
       </div>
     </div>
   );
